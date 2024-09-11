bd4fbb88a9558278c1e7100e72f840b8fe93197f
Daria Doronina <skidle.dd@gmail.com>
2024-09-12 00:55:28 +0200

    oops

--- app/move-to-quiz-folder.js
+++ app/move-to-quiz-folder.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fse = require('fs-extra');
 
 const buildDir = path.join(__dirname, 'build');
-const quizDir = path.join(buildDir, 'quiz');
+const quizDir = path.join(buildDir, 'kviz');
 
 if (fs.existsSync(quizDir)) {
     fse.removeSync(quizDir);
@@ -14,4 +14,4 @@ fs.mkdirSync(quizDir);
 fse.moveSync(path.join(buildDir, 'static'), path.join(quizDir, 'static'));
 fse.moveSync(path.join(buildDir, 'index.html'), path.join(quizDir, 'index.html'));
 
-// fs.writeFileSync(path.join(buildDir, 'index.html'), '<meta http-equiv="refresh" content="0; url=/quiz" />');
+// fs.writeFileSync(path.join(buildDir, 'index.html'), '<meta http-equiv="refresh" content="0; url=/kviz" />');

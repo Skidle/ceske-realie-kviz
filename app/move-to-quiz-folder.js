4f6f8d65112ed51982cbc58cde39d55939b66ce6
Daria Doronina <skidle.dd@gmail.com>
2024-09-12 01:26:43 +0200

    try a main page

--- app/move-to-quiz-folder.js
+++ app/move-to-quiz-folder.js
@@ -4,12 +4,15 @@ const fse = require('fs-extra');
 
 const buildDir = path.join(__dirname, 'build');
 const quizDir = path.join(buildDir, 'kviz');
+const mainPageDir = path.join(__dirname, 'public', 'main');
 
 if (fs.existsSync(quizDir)) {
     fse.removeSync(quizDir);
 }
 
 fs.mkdirSync(quizDir);
+
+fse.removeSync(path.join(buildDir, 'main'));
 
 fs.readdirSync(buildDir).forEach(file => {
     const srcPath = path.join(buildDir, file);
@@ -19,5 +22,7 @@ fs.readdirSync(buildDir).forEach(file => {
         fse.moveSync(srcPath, destPath);
     }
 });
+
+fse.copySync(mainPageDir, buildDir);
 
 console.log('All files moved to "build/kviz" successfully.');
